f762e66d26a52f61f0dbe553db0684e23818b3e6
fajritaridala <fadjritaridala@gmail.com>
2024-12-23 19:06:33 +0800

    delete dummy controller & add a auth controller

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import dummyController from '../controllers/dummy.controller';
+import authController from '../controllers/auth.controller';
 
 const router = express.Router();
 
-router.get('/dummy', dummyController.dummy);
+// registration user
+router.post('/auht/register', authController.register);
 
 export default router;
