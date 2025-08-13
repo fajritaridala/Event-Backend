98ed72be9f7801b066faedaa67735433afebb3f5
fajritaridala <fadjritaridala@gmail.com>
2025-08-13 17:24:42 +0800

    fix: refactor error response

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,11 +1,12 @@
 import { Response } from "express";
+import mongoose from "mongoose";
 import * as Yup from "yup";
 
 type Pagination = {
   totalPages: number;
   current: number;
   total: number;
-}
+};
 
 export default {
   success(res: Response, data: any, message: string) {
@@ -18,15 +19,49 @@ export default {
     });
   },
   error(res: Response, error: unknown, message: string) {
+    // error from yup
     if (error instanceof Yup.ValidationError) {
       return res.status(400).json({
         meta: {
           status: 400,
           message,
         },
-        data: error.errors,
+        data: {
+          [`${error.path}`]: error.errors[0],
+        },
       });
     }
+
+    // error from mongoose
+    if (error instanceof mongoose.Error) {
+      return res.status(500).json({
+        meta: {
+          status: 500,
+          message: error.message,
+        },
+        data: error.name,
+      });
+    }
+
+    if ((error as any)?.code) {
+      const _err = error as any;
+      return res.status(500).json({
+        meta: {
+          status: 500,
+          message: _err.errorResponse.errmsg,
+        },
+        data: _err,
+      });
+    }
+
+    // error default
+    res.status(500).json({
+      meta: {
+        status: 500,
+        message,
+      },
+      data: error,
+    });
   },
   unauthorized(res: Response, message: string = "unauthorized") {
     res.status(403).json({
@@ -37,14 +72,19 @@ export default {
       data: null,
     });
   },
-  pagination(res: Response, data: any[], pagination: Pagination, message: string) {
+  pagination(
+    res: Response,
+    data: any[],
+    pagination: Pagination,
+    message: string
+  ) {
     res.status(200).json({
       meta: {
         status: 200,
         message,
       },
       data,
-      pagination
-    })
+      pagination,
+    });
   },
 };
