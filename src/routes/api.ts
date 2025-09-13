0b5806c2dc84a5d2925e66e3fabef7d78bbb5ffc
fajritaridala <fadjritaridala@gmail.com>
2025-09-13 17:54:27 +0800

    feat: add event api

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,6 +7,7 @@ import mediaMiddleware from "../middlewares/media.middleware";
 import mediaController from "../controllers/media.controller";
 import categoryController from "../controllers/category.controller";
 import regionController from "../controllers/region.controller";
+import eventController from "../controllers/event.controller";
 
 const router = express.Router();
 
@@ -55,11 +56,35 @@ router.delete(
 );
 
 // Region routes
-router.get("/regions", regionController.getAllProvinces);
+router.get(
+  "/regions",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN])],
+  regionController.getAllProvinces
+);
 router.get("/regions/:id/province", regionController.getProvince);
 router.get("/regions/:id/regency", regionController.getRegency);
 router.get("/regions/:id/district", regionController.getDistrict);
 router.get("/regions/:id/village", regionController.getVillage);
 router.get("/regions-search", regionController.findByCity);
+
+// Event routes
+router.post(
+  "/events",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN])],
+  eventController.create
+);
+router.get("/events", eventController.findAll);
+router.get("/events/:id", eventController.findOne);
+router.put(
+  "/events/:id",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN])],
+  eventController.update
+);
+router.delete(
+  "/events/:id",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN])],
+  eventController.remove
+);
+router.get("/events/:slug/slug", eventController.findOneBySlug);
 
 export default router;
